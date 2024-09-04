4bb68432412baf3bd77635c428953308724b09e4
selcuk <yilmax4@gmail.com>
2024-09-04 17:10:16 +0300

    Hydration problem solved this problem very important attention! be careful

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,15 +1,11 @@
+import React, { Suspense } from "react";
 import Header from "@/components/Menu/Header";
 import MobileMenu from "@/components/Menu/MobileMenu";
 import Sidebar from "@/components/Menu/Sidebar";
 import { AuthContext } from "@/context/AuthContext";
 import { QuizProvider } from "@/context/QuizContext";
-import React from "react";
 
-interface RootLayoutProps {
-  children: React.ReactNode;
-}
-
-const RoutesLayout = ({ children }: RootLayoutProps) => {
+const RoutesLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex min-h-screen">
       <AuthContext>
@@ -17,7 +13,9 @@ const RoutesLayout = ({ children }: RootLayoutProps) => {
           <Sidebar />
           <MobileMenu />
           <div className="flex-1 lg:ml-72 ml-0 lg:max-h-screen">
-            <Header />
+            <Suspense fallback={<div>Loading...</div>}>
+              <Header />
+            </Suspense>
             <div className="mx-auto px-5 w-full bg-white-image dark:bg-dark-image bg-cover bg-center bg-no-repeat">
               {children}
             </div>
